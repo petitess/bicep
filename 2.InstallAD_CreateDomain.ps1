014a02adf2db917684ff26dcbddc526c540ad9df
petitess <93782725+petitess@users.noreply.github.com>
2021-11-07 12:25:46 +0100

    Add files via upload

---
+++ 2.InstallAD_CreateDomain.ps1
@@ -0,0 +1,12 @@
+﻿$DSRMPWord = ConvertTo-SecureString -String "12345.abc" -AsPlainText -Force
+
+Write-Verbose "Installing Active Directory Services on this server" -Verbose
+    Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
+    Write-Verbose "Configuring New Domain with on this server" -Verbose
+    Install-ADDSForest -ForestMode WinThreshold -DomainMode WinThreshold -DomainName "karol.se" `
+    -InstallDns -NoDNSonNetwork -SafeModeAdministratorPassword $DSRMPWord -Force -NoRebootOnCompletion
+
+
+  #  Install-ADDSForest -ForestMode WinThreshold -DomainMode WinThreshold -DomainName "karol.se" `
+  # -InstallDns -NoDNSonNetwork -Force
+  # Uninstall-addsforest -forceremoval -demoteoperationmasterrole
