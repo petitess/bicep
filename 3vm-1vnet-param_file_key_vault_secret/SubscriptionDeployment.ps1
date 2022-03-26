dfffbe46c6d1973a1fc25127d60eed8255d24329
Karol <93782725+petitess@users.noreply.github.com>
2022-03-26 14:48:31 +0100

    Description
    This is a simple deployment, using a parameter file.
    If you want to connect to a VM, add RDP to NSG and remove Route Table from Subnet
    If you want to see key vault secrets make your user a Key Vault Administrator
    - One VNET with Subnets connected to NSGs.
    - Route tables
    - 3 VMs with Public IP adress(if set to "true")
    - A key vault containing secrets as windows login
    - A managed identity to run a powershell script to deploy key vault secrets

--- 3vm-1vnet-param_file_key_vault_secret/SubscriptionDeployment.ps1
+++ 3vm-1vnet-param_file_key_vault_secret/SubscriptionDeployment.ps1
@@ -9,3 +9,4 @@ Get-AzContext
 Set-Location C:\3vm-1vnet-param_file
 
 New-AzSubscriptionDeployment -TemplateFile main.bicep -Location "swedencentral" -Name MyDeployment -TemplateParameterFile prod.parameters.json
+#
