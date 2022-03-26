f650cba9c23204cab1a2c83c5671e7041901285b
Karol <93782725+petitess@users.noreply.github.com>
2022-03-26 14:46:28 +0100

    Update SubscriptionDeployment.ps1

--- 3vm-1vnet-param_file_key_vault_secret/SubscriptionDeployment.ps1
+++ 3vm-1vnet-param_file_key_vault_secret/SubscriptionDeployment.ps1
@@ -1,12 +1,12 @@
-﻿clear
+clear
 
 Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 Connect-AzAccount
 Set-AzContext -Subscription 0dcc13b7-1a10-483e-xxxx
-Connect-AzAccount -TenantId e44a6fe3-543e-47c1-a8e6-0ab2841227c8
+Connect-AzAccount -TenantId e44a6fe3-543e-47c1-xxxx
 Get-AzContext
 
-Set-Location C:\Users\karol\Documents\3vm-1vnet-param_file
+Set-Location C:\3vm-1vnet-param_file
 
 New-AzSubscriptionDeployment -TemplateFile main.bicep -Location "swedencentral" -Name MyDeployment -TemplateParameterFile prod.parameters.json
 
