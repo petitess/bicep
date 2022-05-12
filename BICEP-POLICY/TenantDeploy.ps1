50c15888a3bf9d75a3e82834b1bbd57acef54485
Karol <93782725+petitess@users.noreply.github.com>
2022-05-12 17:48:13 +0200

    Add files via upload

---
+++ BICEP-POLICY/TenantDeploy.ps1
@@ -0,0 +1,12 @@
+﻿Clear-Host
+Get-AzPolicyAssignment
+Get-AzContext
+
+set-location C:\Users\SEK\Desktop\03param-file-automation-account\policy
+
+New-AzSubscriptionDeployment -Location swedencentral -Name TenantDeploy -TemplateFile mainpolicy.bicep 
+
+#Allowed resource types
+Get-AzPolicyDefinition -Name a08ec900-254a-4555-9bf5-e42af04b5c5c
+
+Get-AzPolicySetDefinition
