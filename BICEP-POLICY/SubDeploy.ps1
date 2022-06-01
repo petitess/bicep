558f71390b356d6ecbe6538b6d0f6b7213f054b2
Karol <93782725+petitess@users.noreply.github.com>
2022-06-01 14:09:21 +0200

    Add files via upload

---
+++ BICEP-POLICY/SubDeploy.ps1
@@ -0,0 +1,9 @@
+﻿Clear-Host
+Get-AzPolicyAssignment
+Get-AzContext
+
+set-location C:\Users\SEK\Desktop\03param-file-automation-account\BICEP-POLICY
+
+New-AzSubscriptionDeployment -Location swedencentral -Name PolicyDeploy -TemplateFile Allow_env_sub.bicep
+
+New-AzManagementGroupDeployment -Location swedencentral -Name PolicyDeploy -TemplateFile Allow_env_mgmt.bicep -ManagementGroupId MGMT
