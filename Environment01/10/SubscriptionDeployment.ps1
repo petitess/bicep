bca7993c6b7cfa44109377144b839b00ea74f85f
Karol <93782725+petitess@users.noreply.github.com>
2022-06-11 12:18:36 +0200

    Added Log Analytics with alert rules
    This is the 10th version of my test environment.
    Added Log Analytics with alert rules.

--- Environment01/10/SubscriptionDeployment.ps1
+++ Environment01/10/SubscriptionDeployment.ps1
@@ -5,7 +5,7 @@ Set-AzContext -Subscription
 Get-AzContext
 Get-AzSubscription
 
-Set-Location 'C:\Users\KarolSek\OneDrive - B3 Consulting Group AB\bicep'
+Set-Location 'C:\Users'
 
 
 New-AzSubscriptionDeployment -TemplateFile main.bicep -TemplateParameterFile param.json -Location "swedencentral" -Name Deploy$(Get-Date -Format 'yyyy-MM-dd') | select DeploymentName, Location, ProvisioningState, Timestamp, Mode
