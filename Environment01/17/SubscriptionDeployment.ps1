a7eebbf543838b879a7453d7145919b9f55c6a2c
Karol <93782725+petitess@users.noreply.github.com>
2022-07-15 12:52:54 +0200

    Added storage account with container and file shares
    Added storage account with container and file shares

---
+++ Environment01/17/SubscriptionDeployment.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+
+Connect-AzAccount
+Set-AzContext -Subscription 
+Get-AzContext
+Get-AzSubscription
+
+Set-Location ~
+
+New-AzSubscriptionDeployment -TemplateFile main.bicep -TemplateParameterFile param.json -Location "swedencentral" -Name Deploy$(Get-Date -Format 'yyyy-MM-dd') | select DeploymentName, Location, ProvisioningState, Timestamp, Mode
