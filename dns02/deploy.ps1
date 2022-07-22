5acb07f0f733910741933efdfbf47f80d935a0a5
Karol <93782725+petitess@users.noreply.github.com>
2022-07-22 11:30:51 +0200

    Delete dns02 directory

--- dns02/deploy.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location ~"\Desktop\dns02"
-Get-AzContext
-
-New-AzSubscriptionDeployment -Name deploy -TemplateFile .\main.bicep -TemplateParameterFile dev.json -Location swedencentral | Select-Object DeploymentName, ProvisioningState, Timestamp
- 
