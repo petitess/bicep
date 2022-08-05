a922ce2d3454d7c94eec9b355af4c1fab1aab598
Karol <93782725+petitess@users.noreply.github.com>
2022-08-05 09:43:44 +0200

    Microsoft.Network/dnsZones-array

--- dns02/deploy.ps1
+++ dns02/deploy.ps1
@@ -1,5 +1,5 @@
 Set-Location ~"\Desktop\dns02"
 Get-AzContext
-
+Test-AzSubscriptionDeployment -Name deploy -TemplateFile .\main.bicep -TemplateParameterFile dev.json -Location swedencentral
 New-AzSubscriptionDeployment -Name deploy -TemplateFile .\main.bicep -TemplateParameterFile dev.json -Location swedencentral | Select-Object DeploymentName, ProvisioningState, Timestamp
  
