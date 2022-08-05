024ccc199fe2b47f7632d2fb313f74e0545fefc8
Karol <93782725+petitess@users.noreply.github.com>
2022-08-05 09:41:54 +0200

    Delete dns directory

--- dns/deploy.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "C:\Users\KarolSek\Desktop\dns"
-Get-AzContext
-
-New-AzSubscriptionDeployment -Name deploy -TemplateFile .\main.bicep -TemplateParameterFile dev.json -Location swedencentral | Select-Object DeploymentName, ProvisioningState, Timestamp
- 
