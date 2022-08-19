52cc3af0d2b21f42e3e2e629e97041ff0423ed71
Karol <93782725+petitess@users.noreply.github.com>
2022-08-19 09:51:16 +0200

    Delete vgw_connection01 directory

--- vgw_connection01/deploy.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location ~"\Desktop"
-Get-AzContext 
-Set-AzContext -Subscription 
-
-New-AzSubscriptionDeployment -Name deploy -TemplateFile .\main.bicep -TemplateParameterFile param.json -Location swedencentral | Select-Object DeploymentName, ProvisioningState, Timestamp
- 
