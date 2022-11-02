749f6567e01d076f855c41b21d763a1f7b0a5357
Karol <93782725+petitess@users.noreply.github.com>
2022-11-02 04:58:37 +0100

    Google Tag Manager

---
+++ appserviceplan02/SubDeploy.ps1
@@ -0,0 +1,13 @@
+﻿Clear-Host
+
+Connect-AzAccount -Tenant
+Set-AzContext -Subscription
+Get-AzContext
+Get-AzSubscription
+
+Set-Location 'C:\Users\$env:username'
+
+Test-AzSubscriptionDeployment -TemplateFile main.bicep -TemplateParameterFile param.json -Location "swedencentral"
+New-AzSubscriptionDeployment -TemplateFile main.bicep -TemplateParameterFile param.json -Location "swedencentral" -Name Deploy$(Get-Date -Format 'yyyy-MM-dd') | Select-Object DeploymentName, Location, ProvisioningState, Timestamp, Mode
+
+Remove-AzAutoscaleSetting -ResourceGroupName "rg-app-gtm-test-01" -Name "app-gtm-test-02-autoscale01"
