3232e0c864db48232efc174c611d35e05e73ccb6
Karol <93782725+petitess@users.noreply.github.com>
2022-11-05 18:34:48 +0100

    Create maintenance01.ps1

---
+++ xxx01/maintenance01.ps1
@@ -0,0 +1,1 @@
+az vm update --resource-group myResourceGroup --name myVM --set osProfile.windowsConfiguration.enableAutomaticUpdates=true osProfile.windowsConfiguration.patchSettings.patchMode=AutomaticByPlatform
