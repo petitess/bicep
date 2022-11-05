83c23465e1c6f4f3255f149cbe1f960e164feca3
Karol <93782725+petitess@users.noreply.github.com>
2022-11-05 18:15:05 +0100

    VM gallery with VM deployment

---
+++ vmgallery02/SubDeploy.ps1
@@ -0,0 +1,17 @@
+﻿Clear-Host
+
+Connect-AzAccount
+Set-AzContext -Subscription 
+Get-AzContext
+Get-AzSubscription
+
+Set-Location 'C:\Users\$env:username'
+
+Test-AzSubscriptionDeployment -TemplateFile main.bicep -TemplateParameterFile param.json -Location "swedencentral"
+New-AzSubscriptionDeployment -TemplateFile main.bicep -TemplateParameterFile param.json -Location "swedencentral" -Name Deploy$(Get-Date -Format 'yyyy-MM-dd') | Select-Object DeploymentName, Location, ProvisioningState, Timestamp, Mode
+
+Set-AzVm -ResourceGroupName "rg-vmimage01" -Name "vmimage01" -Generalized
+
+(Get-AzVM -ResourceGroupName "rg-vmimage01" -Name "vmimage01" -Status).Statuses[0].DisplayStatus
+
+Get-AzResourceGroup -Name rg-vmimage01 | Remove-AzResourceGroup -Confirm:$false
