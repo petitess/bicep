a78e59ecb17321e6f74a5752a391cb166a4e59ca
Karol <93782725+petitess@users.noreply.github.com>
2023-01-15 17:39:32 +0100

    Azure DevOps

---
+++ pipelines01/01/SubDeployment.ps1
@@ -0,0 +1,9 @@
+﻿$Timestamp = Get-Date -Format 'yyyy-MM-dd_HHmm'
+$ConfigFile =  "./iac/config.json"
+$Config = Get-Content $ConfigFile | ConvertFrom-Json
+$DeploymentName = '{0}_{1}' -f "Infra", $Timestamp
+$ParameterFile = "./iac/param.json"
+
+$Arguments = '--name', $DeploymentName, '--subscription', $Config.subscription, '--location', $Config.location, '--template-file', $Config.templateFile, '--parameters', "@$ParameterFile", '--no-prompt', '--output', 'table'
+
+az deployment sub create $Arguments
